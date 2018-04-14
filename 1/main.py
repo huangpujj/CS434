d1fea1db0cf257eb4d631344af9e673b18632e55
khlam <4841220+khlam@users.noreply.github.com>
2018-04-13 19:52:49 -0700

    doing part 2, added csv data

--- 1/main.py
+++ 1/main.py
@@ -32,56 +32,75 @@ def add_features(f, value = ""):
         array_with_value = np.transpose(np.full((1, len(f)), value, dtype=float))
     return np.hstack((array_with_value, f))
 
-# main
-seed = 69                                               # Random seed for replication
-np.set_printoptions(suppress=True)                      # Turn off scientific notation for CSV
-np.random.seed(seed)                                    
+# Part 2.1 Gradient Descent
+def get_data_csv(filename):
+    rgb = []
+    number = []
+    f = open(filename)
+    for line in f:
+        rgb.append(line.split(",")[0:-1])                      # features in rgb values
+        number.append(line.split(",")[-1].replace("\n",""))    # number they represent
+    return (rgb, number)
 
-(f_train, o_train) = get_data('data/housing_train.txt') # Read training data
-(f_test, o_test) = get_data('data/housing_test.txt')    # Read testing data
+def part1():
+    seed = 69                                               # Random seed for replication
+    np.set_printoptions(suppress=True)                      # Turn off scientific notation for CSV
+    np.random.seed(seed)                                    
 
-w_train = weight(f_train, o_train)                      # Get training data weight
-w_test = weight(f_test, o_test)                         # Get testing data weight
+    (f_train, o_train) = get_data('data/housing_train.txt') # Read training data
+    (f_test, o_test) = get_data('data/housing_test.txt')    # Read testing data
 
-f_dummy_train = add_features(f_train, 1)                # Create dummy column for training data
-w_train_dummy = weight(f_dummy_train, o_train)          # Get weight of dummy
+    w_train = weight(f_train, o_train)                      # Get training data weight
+    w_test = weight(f_test, o_test)                         # Get testing data weight
 
-f_dummy_test = add_features(f_test, 1)                  # Create dummy column for testing data
+    f_dummy_train = add_features(f_train, value = 1)        # Create dummy column for training data
+    f_dummy_test = add_features(f_test, value = 1)          # Create dummy column for testing data
+    w_train_dummy = weight(f_dummy_train, o_train)          # Get weight of dummy
 
-ase_train = ase(f_dummy_train, o_train, w_train_dummy)  # Get ASE of training data with dummy and dummy weight
-ase_test = ase(f_dummy_test, o_test, w_train_dummy)     # Get ASE of testing data with dummy and dummy weight
+    ase_train = ase(f_dummy_train, o_train, w_train_dummy)  # Get ASE of training data with dummy and dummy weight
+    ase_test = ase(f_dummy_test, o_test, w_train_dummy)     # Get ASE of testing data with dummy and dummy weight
 
-print ("\n=====================================\n")
-print ("Weight Vector with Dummy Column\n" + str(w_train_dummy))    # Part 1.1
-print ("\nTraining ASE:\t" + str(ase_train))                        # Part 1.2
-print ("Testing ASE:\t" + str(ase_test))                            # Part 1.2
+    print ("\n=====================================\n")
+    print ("Weight Vector with Dummy Column\n" + str(w_train_dummy))    # Part 1.1.1
+    print ("\nTraining ASE:\t" + str(ase_train))                        # Part 1.1.2
+    print ("Testing ASE:\t" + str(ase_test))                            # Part 1.1.2
 
-ase_train = ase(f_train, o_train, w_train)          # Get ASE of training data with training data weight (no dummy)
-ase_test = ase(f_test, o_test, w_train)             # Get ASE of testing data with training data weight  (no dummy)
+    ase_train = ase(f_train, o_train, w_train)          # Get ASE of training data with training data weight (no dummy)
+    ase_test = ase(f_test, o_test, w_train)             # Get ASE of testing data with training data weight  (no dummy)
 
-print ("\n=====================================\n")
-print ("Weight Vector without Dummy Column\n" + str(w_train))   # Part 3.1 Remove dummy column
-print ("\nTraining ASE:\t" + str(ase_train))                    # Part 3.2
-print ("Testing ASE:\t" + str(ase_test))                        # Part 3.2
+    print ("\n=====================================\n")
+    print ("Weight Vector without Dummy Column\n" + str(w_train))   # Part 1.3.1 Remove dummy column
+    print ("\nTraining ASE:\t" + str(ase_train))                    # Part 1.3.2
+    print ("Testing ASE:\t" + str(ase_test))                        # Part 1.3.2
+
+    # Part 1.4
+    # Iterate from adding 2 to 100 random features and print ASE for training and testing
+    print ("\n=====================================\n")
+    print ("Random seed: " + str(seed))
+    print("\td\tTraining ASE\tTesting ASE")
+    f = open("ase.csv", 'w+')
+    f.write("d,Training ASE,Testing ASE\n")
+    for d in range(2, 100, 2):
+        n_train_feature = f_train
+        n_test_feature = f_test
+        for j in range(1, d):
+            n_train_feature = add_features(n_train_feature)
+            n_test_feature = add_features(n_test_feature)
+        n_train_weight = weight(n_train_feature, o_train)
+        n_ase_train = ase(n_train_feature, o_train, n_train_weight)
+        n_ase_test = ase(n_test_feature, o_test, n_train_weight)
+        print("\t" + str(d) + "\t" + str(n_ase_train) + "\t" + str(n_ase_test))
+        f.write(str(d) + "," + str(n_ase_train) + "," + str(n_ase_test) + "\n")
+    f.close()
+    print ("\n=====================================\n")
+
+def part2():
+    (rgb, number) = get_data_csv("data/usps-4-9-train.csv")
+    print(str(rgb))
+    print(str(number))
 
 
-# Part 4
-# Iterate from adding 2 to 100 random features and print ASE for training and testing
-print ("\n=====================================\n")
-print ("Random seed: " + str(seed))
-print("\td\tTraining ASE\tTesting ASE")
-f = open("ase.csv", 'w+')
-f.write("d,Training ASE,Testing ASE\n")
-for d in range(2, 100, 2):
-    n_train_feature = f_train
-    n_test_feature = f_test
-    for j in range(1, d):
-        n_train_feature = add_features(n_train_feature)
-        n_test_feature = add_features(n_test_feature)
-    n_train_weight = weight(n_train_feature, o_train)
-    n_ase_train = ase(n_train_feature, o_train, n_train_weight)
-    n_ase_test = ase(n_test_feature, o_test, n_train_weight)
-    print("\t" + str(d) + "\t" + str(n_ase_train) + "\t" + str(n_ase_test))
-    f.write(str(d) + "," + str(n_ase_train) + "," + str(n_ase_test) + "\n")
-f.close()
-print ("\n=====================================\n")
+#part1()
+
+part2()
+
