8d0b8b97f25f4c169e5bc7d85397af33e02986dc
khlam <4841220+khlam@users.noreply.github.com>
2018-04-26 22:34:46 -0700

    corrected get_data()

--- 2/knn.py
+++ 2/knn.py
@@ -6,10 +6,10 @@ def get_data(filename):
     outputs = []
     f = open(filename, 'r')
     for line in f:
-        features.append(line.split()[0:-1]) # Column 1-13 are features (crime rate, accessibility etc)
-        outputs.append(line.split()[-1])    # Column 14 is the goal (median housing value)
+        features.append(line.split(",")[0])
+        outputs.append(line.split(",")[1:-1])
     f.close()
-    (features, outputs) = (np.array(features, dtype=float), np.array(outputs, dtype=float)) # Ensure all values are floats
+    (features, outputs) = (np.array(features, dtype=float), np.array(outputs, dtype=float))
     return (features, outputs)
 
 print get_data('data/knn_train.csv')
