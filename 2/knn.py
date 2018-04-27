4ec395f189ea7cb41df0b4e727624b2fe027f0a2
khlam <4841220+khlam@users.noreply.github.com>
2018-04-26 22:50:25 -0700

    messing around

--- 2/knn.py
+++ 2/knn.py
@@ -1,15 +1,22 @@
 import numpy as np
-import random
 
 def get_data(filename):
+    diagnosis = []
     features = []
-    outputs = []
     f = open(filename, 'r')
     for line in f:
-        features.append(line.split(",")[0])
-        outputs.append(line.split(",")[1:-1])
+        diagnosis.append(line.split(",")[0])    # Diagnosis in this array
+        features.append(line.split(",")[1:-1])  # Features in this array
     f.close()
-    (features, outputs) = (np.array(features, dtype=float), np.array(outputs, dtype=float))
-    return (features, outputs)
+    (diagnosis, features) = (np.array(diagnosis, dtype=int), np.array(features, dtype=float))
+    return (diagnosis, features)
 
-print get_data('data/knn_train.csv')
+def dist(x, y):                 # Distance Function
+    return np.linalg.norm(x-y)
+
+(diagnosis, features) = get_data('data/knn_train.csv')
+
+print("Diagnosis: " + str(diagnosis))   # Sanity check
+print("Features: " + str(features) + "\n")
+
+print dist(features[0], features[1])
