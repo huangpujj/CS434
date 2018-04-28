d0877f80f1bf406458d8c6043cbaf12c2260836e
khlam <4841220+khlam@users.noreply.github.com>
2018-04-28 14:54:41 -0700

    rewrite based on new pseudocode

--- 2/knn.py
+++ 2/knn.py
@@ -10,14 +10,15 @@ def get_data(filename):
     f = open(filename, 'r')
     for line in f:
         diagnosis.append(line.split(",")[0])    # Diagnosis in this array
-        features.append(line.split(",")[1:-1])  # Features in this array
+        features.append(line.split(",")[1:])  # Features in this array
     f.close()
     (diagnosis, features) = (np.array(diagnosis, dtype=int), np.array(features, dtype=float))
-    return (diagnosis, features)
+    return diagnosis, features
 
 # Euclidian distance
-def distance(x, xi):                 
-    return np.linalg.norm(x-xi)
+# https://stackoverflow.com/questions/4370975/python-numpy-euclidean-distance-calculation-between-matrices-of-row-vectors
+def distance(x, xi):
+    return np.sqrt( np.sum((x - xi)**2, axis=1) )
 
 # Feature scaling normalization https://en.wikipedia.org/wiki/Feature_scaling
 def normalize(x):
@@ -26,22 +27,28 @@ def normalize(x):
     return (x - x_min) / (x_max - x_min)
 
 # KNN 
-def knn(train_f, test_f):
+def knn(feature, diagnosis, features, k):
     dist = []
-    for i in range(0, len(train_f)):                                    # For each training feature row, calculate its distance 
-        dist.append( (train_f[i], distance(test_f, train_f[i])) )
-    return sorted(dist,key=lambda x:(-x[1],x[0]))                       # Sort by distance in descending order
+    d_arr = distance(feature, features)
+    for i in range(len(features)):
+        dist.append( (diagnosis[i], d_arr[i]) )
+    sorted_dist = sorted(dist,key=lambda x:(x[1],-x[0]))
+    return sorted_dist
 
-(train_d, train_f) = get_data('data/knn_train.csv')     # Get training set data
-(test_d, test_f) = get_data('data/knn_test.csv')        # Get testing set data
+def classify(feature, diagnosis, features, k):
+    _knn = knn(feature, diagnosis, features, k)
+
+def training_error(train_d, train_f, test_d, test_f, K):
+    predict = []
+    for i in range(len(train_f)):
+        predict.append( classify(train_f[i], train_d, train_f, K) )
+
+train_d, train_f = get_data('data/knn_train.csv')     # Get training set data
+test_d, test_f = get_data('data/knn_test.csv')        # Get testing set data
 
 train_f = normalize(train_f)
 test_f = normalize(test_f)
-print knn(train_f, test_f)
-'''
 
-_knn = []
-for row in enumerate(test_f[0:-1]):
-    _knn.append(knn(train_f, row[1]))
-'''
-#print _knn
+K = range(1, 51, 2) # K values 1, 3, 5 ... 51
+
+print("\tTraining Error:\t" + str(training_error(train_d, train_f, test_d, test_f, K) ) )
