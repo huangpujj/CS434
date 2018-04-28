e89400cd6038c3b79ba4244fa4dc832afcdb5059
khlam <4841220+khlam@users.noreply.github.com>
2018-04-28 12:06:11 -0700

    need to test this with simpler dataset

--- 2/knn.py
+++ 2/knn.py
@@ -1,5 +1,9 @@
 import numpy as np
+import operator
+from operator import itemgetter
 
+# Fetches data from filename and returns a tuple (diagnosis, features) where 
+# diagnosis and features are arrays
 def get_data(filename):
     diagnosis = []
     features = []
@@ -11,12 +15,33 @@ def get_data(filename):
     (diagnosis, features) = (np.array(diagnosis, dtype=int), np.array(features, dtype=float))
     return (diagnosis, features)
 
-def dist(x, y):                 # Distance Function
-    return np.linalg.norm(x-y)
+# Euclidian distance
+def distance(x, xi):                 
+    return np.linalg.norm(x-xi)
 
-(diagnosis, features) = get_data('data/knn_train.csv')
+# Feature scaling normalization https://en.wikipedia.org/wiki/Feature_scaling
+def normalize(x):
+    x_max = np.amax(x, axis=0)
+    x_min = np.amin(x, axis=0)     
+    return (x - x_min) / (x_max - x_min)
 
-print("Diagnosis: " + str(diagnosis))   # Sanity check
-print("Features: " + str(features) + "\n")
+# KNN 
+def knn(train_f, test_f):
+    dist = []
+    for i in range(0, len(train_f)):                                    # For each training feature row, calculate its distance 
+        dist.append( (train_f[i], distance(test_f, train_f[i])) )
+    return sorted(dist,key=lambda x:(-x[1],x[0]))                       # Sort by distance in descending order
 
-print dist(features[0], features[1])
+(train_d, train_f) = get_data('data/knn_train.csv')     # Get training set data
+(test_d, test_f) = get_data('data/knn_test.csv')        # Get testing set data
+
+train_f = normalize(train_f)
+test_f = normalize(test_f)
+print knn(train_f, test_f)
+'''
+
+_knn = []
+for row in enumerate(test_f[0:-1]):
+    _knn.append(knn(train_f, row[1]))
+'''
+#print _knn
