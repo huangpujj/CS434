35db9e203b6233e5a19d448d7af15e6e5b6d7535
khlam <4841220+khlam@users.noreply.github.com>
2018-04-28 16:31:59 -0700

    training error from 1 to 51

--- 2/knn.py
+++ 2/knn.py
@@ -27,21 +27,31 @@ def normalize(x):
     return (x - x_min) / (x_max - x_min)
 
 # KNN 
-def knn(feature, diagnosis, features, k):
-    dist = []
-    d_arr = distance(feature, features)
-    for i in range(len(features)):
-        dist.append( (diagnosis[i], d_arr[i]) )
-    sorted_dist = sorted(dist,key=lambda x:(x[1],-x[0]))
-    return sorted_dist
+def knn(feature, diag_set, feat_set, k):
+    dist = []                                               # Array that holds all pairs (feature, distance)
+    d_arr = distance(feature, feat_set)                     # Calculate the distance from specific `feature` 
+    for i in range(len(feat_set)):                          # to all other features.
+        dist.append( (diag_set[i], d_arr[i]) )              # Put all features and distances in pairs
+    sorted_dist = sorted(dist,key=lambda x:(x[1],-x[0]))    # Sort all paris in asceending order by distance
+    k_sorted = sorted_dist[:k]                              # Return the closest k pairs out
+    return k_sorted
 
-def classify(feature, diagnosis, features, k):
-    _knn = knn(feature, diagnosis, features, k)
+# Determines what diagnosis `feature` is based on its k closest neighbors
+def classify(feature, diag_set, feat_set, k):
+    _knn = knn(feature, diag_set, feat_set, k)      # Get all closest neighbors.
+    total = 0
+    for i in range(len(_knn)):                      # Sum all neighbors.
+        total += _knn[i][0]                         # If majority of neighbors are
+    if total < 0:                                   # negative then total will be less
+        return -1                                   # than 0, so we assume `feature`
+    else:                                           # is also negative. Otherwise assume
+        return 1                                    # it's positive
 
-def training_error(train_d, train_f, test_d, test_f, K):
+def training_error(diag_set, feat_set, K):
     predict = []
-    for i in range(len(train_f)):
-        predict.append( classify(train_f[i], train_d, train_f, K) )
+    for i in range(len(feat_set)):
+        predict.append( classify(feat_set[i], diag_set, feat_set, K) )
+    return np.sum(np.abs(predict - diag_set)) / float(2 * len(diag_set)) * 100
 
 train_d, train_f = get_data('data/knn_train.csv')     # Get training set data
 test_d, test_f = get_data('data/knn_test.csv')        # Get testing set data
@@ -49,6 +59,6 @@ test_d, test_f = get_data('data/knn_test.csv')        # Get testing set data
 train_f = normalize(train_f)
 test_f = normalize(test_f)
 
-K = range(1, 51, 2) # K values 1, 3, 5 ... 51
-
-print("\tTraining Error:\t" + str(training_error(train_d, train_f, test_d, test_f, K) ) )
+for k in range(1, 53, 2): # K values 1, 3, 5 ... 51
+    print("\tK = " + str(k))
+    print("\tTraining Error:\t" + str( training_error(train_d, train_f, k) ) + str("%") )
