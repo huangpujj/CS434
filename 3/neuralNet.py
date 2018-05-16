a5f84c9e9ff3742c29076c8dbf11c83900abc25a
khlam <4841220+khlam@users.noreply.github.com>
2018-05-15 22:53:45 -0700

    fixing for part 1

--- 3/neuralNet.py
+++ 3/neuralNet.py
@@ -40,26 +40,24 @@ validation_loader = torch.utils.data.DataLoader(
 class Network(nn.Module):
 	def __init__(self):
 		super(Network, self).__init__()
-		self.fc1 = nn.Linear(3*32*32, 100)	# Hidden layer
-		self.fc1Drop = nn.Dropout(0.2)		# Some regularization
+		self.fc1 = nn.Linear(3*32*32, 100)	
+		self.fc1Drop = nn.Dropout(0.2)	
 		self.fc2 = nn.Linear(100, 10)
-		self.fcm1 = nn.Linear(3*32*32, 50)
-		self.fcm2 = nn.Linear(50,50)
-		self.fcm3 = nn.Linear(50,10)
+		#self.fcm1 = nn.Linear(3*32*32, 50)
+		#self.fcm2 = nn.Linear(50,50)
+		#self.fcm3 = nn.Linear(50,10)
 		
 	def sigmoid(self, x):
 		x = x.view(-1, 3*32*32)
 		x = F.sigmoid(self.fc1(x))
 		x = self.fc1Drop(x)
-		x = F.sigmoid(self.fc2(x))
-		return x
+		return F.log_softmax(self.fc2(x))
 	
 	def relu(self,x):
 		x = x.view(-1, 3*32*32)
 		x = F.relu(self.fc1(x))
 		x = self.fc1Drop(x)
-		x = F.relu(self.fc2(x))
-		return x
+		return F.log_softmax(self.fc2(x))
 
 	def multi_layer(self,x):
 		x = x.view(-1, 3*32*32)
@@ -92,6 +90,9 @@ def validate_sigmoid(model, optimizer, sig_accuracy, sig_avg_loss,loss_vector, a
 	model.eval()
 	val_loss, correct = 0, 0
 	for data, target in validation_loader:
+		if cuda:
+			data, target = data.cuda(), target.cuda()
+		data, target = Variable(data), Variable(target)
 		output = model.sigmoid(data)
 		val_loss += F.nll_loss(output, target).data.item()
 		pred = output.data.max(1)[1]
@@ -311,7 +312,7 @@ def part4():
 			validate_multi(model, optimizer, part4, lossv, accv)
 		part4.write("\n")
 	part4.close()
-'''
+
 print("\t--- Part 1 Start ---\n")
 part1()
 print("\t--- Part 1 End ---\n")
@@ -319,7 +320,7 @@ print("\t--- Part 1 End ---\n")
 print("\t--- Part 2 Start ---\n")
 part2()
 print("\t--- Part 2 End ---\n")
-'''
+
 print("\t--- Part 3 Start ---\n")
 part3()
 print("\t--- Part 3 End ---\n")
