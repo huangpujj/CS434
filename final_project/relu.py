114396046e3ee75e569f5fc12b7b9dc0eb1928d0
khlam <4841220+khlam@users.noreply.github.com>
2018-06-06 13:40:28 -0700

    still not quite working

--- final_project/relu.py
+++ final_project/relu.py
@@ -7,13 +7,16 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable	# Deprecated
+from torch.utils.data import Dataset, DataLoader
+
+import itertools
 
 import numpy as np
 
 # --- Global Statements Start ---
 learningRates = [0.0001, 0.001, 0.01, 0.1]
 epochs = 10
-batch_size = 100
+batch_size = 32
 
 if (len(sys.argv) > 1):
 	DROPOUT = float(sys.argv[1])
@@ -28,42 +31,77 @@ cuda = torch.cuda.is_available()
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
 
-data = datasets.CIFAR10(root='cifar', train=True, download=True,
-                    transform=transforms.ToTensor()).train_data
-data = data.astype(np.float32)/255.
 
-means = []
-stdevs = []
-for i in range(3):
-    pixels = data[:,i,:,:].ravel()
-    means.append(np.mean(pixels))
-    stdevs.append(np.std(pixels))
+class DiabetesDataset(Dataset):
+	""" Diabetes dataset."""
 
-print("CIFAR10 Mean: " + str(means))
-print("CIFAR10 Standard Deviation: " + str(stdevs))
+	# Initialize data
+	def __init__(self):
+		xy = np.loadtxt('./data/part1/Subject_2_part1.csv', delimiter=',', usecols=(1, 2, 3, 4, 5, 6, 7, 8, 9), dtype=np.float32)
+		self.len = xy.shape[0]
+		batch = torch.tensor((), dtype=torch.float64)
+		diag = torch.tensor((), dtype=torch.float64)
+		
+		concat_batch = []
+		concat_diag = []
+		
+		for i, row in enumerate(xy):
+			new_batch = []			
+			
+			if i+7 <= self.len:
+				for j in range(i, i+7):
+					new_batch = [x for x in itertools.chain(new_batch, xy[j, 0:8])]
+					# new_batch.append(xy[j, 0:8])
+					#new_tensor = torch.from_numpy(xy[j, 0:8])			# not including hypo
+					#new_batch = torch.cat((new_batch, new_tensor), 0)
+					if j == i+6:
+						last = xy[j, [-1]]
 
-train_loader = torch.utils.data.DataLoader(
-	datasets.CIFAR10('../data', train=True, download=True,
-				   transform=transforms.Compose([
-					   transforms.ToTensor(),
-					   transforms.Normalize(torch.FloatTensor(means), torch.FloatTensor(stdevs))
-				   ])),
-	batch_size=batch_size, shuffle=True, num_workers=2)
+			concat_batch.append(new_batch)
+			concat_diag = np.append(concat_diag, last)
 
-validation_loader = torch.utils.data.DataLoader(
-	datasets.CIFAR10('../data', train=False, download=True, transform=transforms.Compose([
-					   transforms.ToTensor(),
-					   transforms.Normalize(torch.FloatTensor(means), torch.FloatTensor(stdevs))
-				   ])),
-	batch_size=batch_size, shuffle=False, num_workers=2)
+			if i+7 == 11: # For testing purposes
+				break
 
+		concat_batch = np.asarray(concat_batch)
+
+		batch = torch.tensor(torch.from_numpy(concat_batch), dtype=torch.float64)
+		diag = torch.tensor(torch.from_numpy(concat_diag), dtype=torch.float64)
+
+		self.x_data = batch
+		self.y_data = diag
+
+	def __getitem__(self, index):
+		return self.x_data[index], self.y_data[index]
+
+	def __len__(self):
+		return self.len
+
+
+dataset = DiabetesDataset()
+train_loader = DataLoader(dataset=dataset,
+						  batch_size=1,
+						  shuffle=False,
+						  num_workers=2)
+'''
+for epoch in range(2):
+	for i, data in enumerate(train_loader, 0):
+		# get the inputs
+		inputs, labels = data
+
+		# wrap them in Variable
+		inputs, labels = Variable(inputs), Variable(labels)
+
+		# Run your training process
+print(epoch, i, "inputs", inputs.data, "labels", labels.data)
+'''
 # --- Global Statements End---
 
 # https://pytorch.org/docs/master/nn.html
 class Network(nn.Module):
 	def __init__(self):
 		super(Network, self).__init__()
-		self.fc1 = nn.Linear(3*32*32, 100)	
+		self.fc1 = nn.Linear(32, 100)	
 		self.fc1Drop = nn.Dropout(DROPOUT)	
 		self.fc2 = nn.Linear(100, 10)
 
@@ -78,7 +116,7 @@ class Network(nn.Module):
 		return F.log_softmax(self.fc2(x))
 	
 	def relu(self,x):
-		x = x.view(-1, 3*32*32)
+		x = x.view(-1, 32)
 		x = F.relu(self.fc1(x))
 		x = self.fc1Drop(x)
 		return F.log_softmax(self.fc2(x))
@@ -107,8 +145,8 @@ def train_relu(model, optimizer, epoch, log_interval = 100):
 		optimizer.step()
 		if batch_idx % log_interval == 0:
 			print('RELU -- Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data.item()))
+				epoch, batch_idx * len(data), len(train_loader.dataset),
+				100. * batch_idx / len(train_loader), loss.data.item()))
 	
 def validate_relu(model, optimizer, relu_accuracy, relu_avg_loss, loss_vector, accuracy_vector):
 	model.eval()
@@ -163,7 +201,7 @@ def part2():
 		lossv, accv = [], []
 		for epoch in range(1, epochs + 1):
 			train_relu(model, optimizer, epoch)
-			validate_relu(model, optimizer, p2_relu_acc, p2_relu_avg_loss, lossv, accv)
+			# validate_relu(model, optimizer, p2_relu_acc, p2_relu_avg_loss, lossv, accv)
 		p2_relu_acc.write("\n")
 		p2_relu_avg_loss.write("\n")
 
@@ -171,6 +209,4 @@ def part2():
 	p2_relu_avg_loss.close()
 
 
-print("\t--- Part 2 Start ---\n")
 part2()
-print("\t--- Part 2 End ---\n")
