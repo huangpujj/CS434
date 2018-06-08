8259bfefcb1b33f8994c9e990dd7daa830d29a98
khlam <4841220+khlam@users.noreply.github.com>
2018-06-08 15:51:41 -0700

    new function to load files

--- final_project/nn.py
+++ final_project/nn.py
@@ -35,7 +35,7 @@ class DiabetesDataset(Dataset):
 	def get_indice(self, indice = False):
 		self.indice = []
 		if indice is not False:
-			for line in open(indice):
+			for line in indice:
 				self.indice.append(int(line))
 	
 	# Checks if window[start:end] is a continuous block
@@ -52,20 +52,19 @@ class DiabetesDataset(Dataset):
 		return True						# Window is continuous
 
 	def __init__(self, data, indice):
-		xy = np.loadtxt(data, delimiter=',', usecols=(1, 2, 3, 4, 5, 6, 7, 8, 9), dtype=np.float32)
-		data_total_len = xy.shape[0]
+		data_total_len = data.shape[0]
 		self.get_indice(indice)
 
 		batch = []
 		labels = []
 
-		for i, row in enumerate(xy):
+		for i, row in enumerate(data):
 			new_batch = []
 			if  i+7 <= data_total_len and self.check_window(i, i+7):
 				for j in range(i, i+7):
-					new_batch = [x for x in itertools.chain(new_batch, xy[j, 0:8])]
+					new_batch = [x for x in itertools.chain(new_batch, data[j, 0:8])]
 					if j == i+6:
-						last = xy[j, [-1]]
+						last = data[j, [-1]]
 				batch.append(new_batch)
 				labels = np.append(labels, last)
 			else:
@@ -78,15 +77,15 @@ class DiabetesDataset(Dataset):
 		self.len = self.x_data.shape[0]
 		# print self.x_data
 		# print self.y_data
-		#print self.x_data.shape
-		#print self.y_data.shape
-		#print "\n\n"
+		# print self.x_data.shape
+		# print self.y_data.shape
+		# print "\n\n"
 
 	def __getitem__(self, index):
 		window = torch.tensor(torch.from_numpy(self.x_data[index]))
 		label = torch.tensor(self.y_data[index]).long()
-		#print window.type()
-		#print label
+		# print window.type()
+		# print label
 		return window, label
 
 	def __len__(self):
@@ -111,7 +110,6 @@ class Net(nn.Module):
 		#return F.log_softmax(out)
 
 def train(model, epoch, log_interval = 100):
-	#model.train()
 	for batch_idx, (data, target) in enumerate(train_loader):
 		if cuda:
 			data, target = data.cuda(), target.cuda()
@@ -131,7 +129,6 @@ def train(model, epoch, log_interval = 100):
 				100. * batch_idx / len(train_loader), loss.data.item()))
 	
 def validate(model):
-	#model.eval()
 	total, correct = 0, 0
 	for data, labels in validation_loader:
 		outputs = model(data)
@@ -142,19 +139,27 @@ def validate(model):
 	print('    Total:	%d' % total)
 	print(' Accuracy:	%d %%' % (100 * correct / total))
 
+def kFold(data_file, indice_file):
+	data = np.loadtxt(data_file, delimiter=',', usecols=(1, 2, 3, 4, 5, 6, 7, 8, 9), dtype=np.float32)
+	with open(indice_file, 'r') as f:
+   		indice = [line.strip() for line in f]
+	return data, indice
 
-dataset = DiabetesDataset('./data/part1/Subject_2_part1.csv', './data/part1/list2_part1.csv')
-train_loader = DataLoader(dataset=dataset,
+data, indice = kFold('./data/part1/Subject_2_part1.csv', './data/part1/list2_part1.csv')
+
+train_set = DiabetesDataset(data, indice)
+
+train_loader = DataLoader(dataset=train_set,
 						  batch_size=1,
-						  shuffle=False,
+						  shuffle=True,
 						  num_workers=2)
-
-dataset1 = DiabetesDataset('./data/part1/Subject_7_part1.csv', './data/part1/list_7_part1.csv')
-validation_loader = DataLoader(dataset=dataset1,
+'''
+test_set = DiabetesDataset('./data/part1/Subject_7_part1.csv', './data/part1/list_7_part1.csv')
+validation_loader = DataLoader(dataset=test_set,
 						  batch_size=1,
 						  shuffle=False,
 						  num_workers=2) 
-
+'''
 model = Net(input_size, hidden_size_1, hidden_size_2, num_classes)
 if cuda:
 	model.cuda()
@@ -169,6 +174,6 @@ def main():
 	for epoch in range(1, epochs + 1):
 		print("\tEpoch\t\tInterval\t\tLoss")
 		train(model, epoch)
-		validate(model)
+		#validate(model)
 
 main()
