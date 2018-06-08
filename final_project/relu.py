47077066337159b88edf396433a8f69eb2f6c513
khlam <4841220+khlam@users.noreply.github.com>
2018-06-08 12:26:40 -0700

    training

--- final_project/relu.py
+++ final_project/relu.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
-from torch.autograd import Variable	# Deprecated
+from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader
 
 import itertools
@@ -14,7 +14,7 @@ import itertools
 import numpy as np
 
 # --- Global Statements Start ---
-learningRates = [0.1]
+learningRate = 0.1
 epochs = 1
 batch_size = 32
 
@@ -57,8 +57,6 @@ class DiabetesDataset(Dataset):
 	def __init__(self, data, indice):
 		xy = np.loadtxt(data, delimiter=',', usecols=(1, 2, 3, 4, 5, 6, 7, 8, 9), dtype=np.float32)
 		
-		self.len = xy.shape[0]
-
 		self.get_indice(indice)
 
 		batch = []
@@ -66,7 +64,7 @@ class DiabetesDataset(Dataset):
 
 		for i, row in enumerate(xy):
 			new_batch = []
-			if  i+7 <= self.len and self.check_window(i, i+7):
+			if  i+7 <= xy.shape[0] and self.check_window(i, i+7):
 				for j in range(i, i+7):
 					new_batch = [x for x in itertools.chain(new_batch, xy[j, 0:8])]
 					if j == i+6:
@@ -80,17 +78,16 @@ class DiabetesDataset(Dataset):
 
 		self.x_data = batch
 		self.y_data = labels
-		
-		print self.x_data
-		print self.y_data
+		self.len = self.x_data.shape[0]
+		# print self.x_data
+		# print self.y_data
 		print self.x_data.shape
 		print self.y_data.shape
 		print "\n\n"
-		
 
 	def __getitem__(self, index):
 		window = torch.tensor(torch.from_numpy(self.x_data[index]))
-		label = torch.tensor(self.y_data[index], dtype=torch.float64)
+		label = torch.tensor(self.y_data[index]).long()
 		#print window.type()
 		#print label
 		return window, label
@@ -104,47 +101,50 @@ train_loader = DataLoader(dataset=dataset,
 						  batch_size=1,
 						  shuffle=False,
 						  num_workers=1)
-'''
-for epoch in range(2):
-	for i, data in enumerate(train_loader, 0):
-		# get the inputs
-		inputs, labels = data
-
-		# wrap them in Variable
-		inputs, labels = Variable(inputs), Variable(labels)
-
-		# Run your training process
-print(epoch, i, "inputs", inputs.data, "labels", labels.data)
-'''
+						  
 # --- Global Statements End---
 
 # https://pytorch.org/docs/master/nn.html
 class Network(nn.Module):
 	def __init__(self):
 		super(Network, self).__init__()
-		self.fc1 = nn.Linear(1, 7*8*1)	
+		self.fc1 = nn.Linear(7*8, 7*8)	
 		self.fc1Drop = nn.Dropout(DROPOUT)	
-		self.fc2 = nn.Linear(7*8*1, 7*8*1)
+		self.fc2 = nn.Linear(7*8, 8)
+		self.relu_1 = nn.ReLU()
 	
 	def relu(self, x):
-		x = x.view(-1, 7*8*1)
-		x = F.relu(self.fc1(x))
+		#x = x.view(-1, 7*8)
+		x = self.fc1(x)
+		x = self.relu_1(x)
 		x = self.fc1Drop(x)
-		return F.log_softmax(self.fc2(x))
+		x = self.fc2(x)
+		return F.log_softmax(x)
 
 # --- Relu Functions Start ---
 
-def train_relu(model, optimizer, epoch, log_interval = 100):
+model = Network()
+if cuda:
+	model.cuda()
+criterion = nn.CrossEntropyLoss()  
+#optimizer = optim.SGD(model.parameters(), lr=rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=learningRate)  
+
+def train_relu(model, epoch, log_interval = 100):
 	model.train()
 	for batch_idx, (data, target) in enumerate(train_loader):
 		if cuda:
 			data, target = data.cuda(), target.cuda()
+		# Convert torch tensor to variable
 		data, target = Variable(data), Variable(target)
-		optimizer.zero_grad()
+		
+		# Forward + backward + optimize
+		optimizer.zero_grad()	# zero the gradient buffer
 		output = model.relu(data)
-		loss = F.nll_loss(output, target)
+		loss = criterion(output, target)
 		loss.backward()
 		optimizer.step()
+		
 		if batch_idx % log_interval == 0:
 			print('RELU -- Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
 				epoch, batch_idx * len(data), len(train_loader.dataset),
@@ -155,7 +155,7 @@ def validate_relu(model, optimizer, loss_vector, accuracy_vector):
 	val_loss, correct = 0, 0
 	for data, target in validation_loader:
 		output = model.relu(data)
-		val_loss += F.nll_loss(output, target).data.item()
+		val_loss += criterion(output, target).data.item()
 		pred = output.data.max(1)[1]
 		correct += pred.eq(target.data).cpu().sum()
 
@@ -169,19 +169,12 @@ def validate_relu(model, optimizer, loss_vector, accuracy_vector):
 
 # --- Relu Functions End ---
 
-def main():
-	for rate in learningRates:
-		model = Network()
-		if cuda:
-			model.cuda()
-			
-		print("Learning Rate: " + str(rate))
+def main():		
+	print("Learning Rate: " + str(learningRate))
 
-		optimizer = optim.SGD(model.parameters(), lr=rate)
-
-		lossv, accv = [], []
-		for epoch in range(1, epochs + 1):
-			train_relu(model, optimizer, epoch)
+	lossv, accv = [], []
+	for epoch in range(1, epochs + 1):
+		train_relu(model, epoch)
 			# validate_relu(model, optimizer, lossv, accv)
 
 main()
