008f5e2c5606c0f08f20da7e0d0b655edab78d5b
khlam <4841220+khlam@users.noreply.github.com>
2018-06-08 16:03:46 -0700

    more refactor

--- final_project/nn.py
+++ final_project/nn.py
@@ -31,47 +31,7 @@ cuda = torch.cuda.is_available()
 kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
 
 class DiabetesDataset(Dataset):
-	# Retrieves all indices
-	def get_indice(self, indice = False):
-		self.indice = []
-		if indice is not False:
-			for line in indice:
-				self.indice.append(int(line))
-	
-	# Checks if window[start:end] is a continuous block
-	def check_window(self, start, end):
-		if len(self.indice) != 0:
-			array = self.indice[start:end]
-			for i, x in enumerate(array):
-				if i + 1 < len(array):
-					temp = x + 1
-					if temp == array[i+1]:
-						continue
-					else:
-						return False	# Window is not continuous
-		return True						# Window is continuous
-
-	def __init__(self, data, indice):
-		data_total_len = data.shape[0]
-		self.get_indice(indice)
-
-		batch = []
-		labels = []
-
-		for i, row in enumerate(data):
-			new_batch = []
-			if  i+7 <= data_total_len and self.check_window(i, i+7):
-				for j in range(i, i+7):
-					new_batch = [x for x in itertools.chain(new_batch, data[j, 0:8])]
-					if j == i+6:
-						last = data[j, [-1]]
-				batch.append(new_batch)
-				labels = np.append(labels, last)
-			else:
-				continue			# If window size is not 7 or if the contents of the window is not continuous, skip
-
-		batch = np.array(batch)
-
+	def __init__(self, batch, labels):
 		self.x_data = batch
 		self.y_data = labels
 		self.len = self.x_data.shape[0]
@@ -139,22 +99,69 @@ def validate(model):
 	print('    Total:	%d' % total)
 	print(' Accuracy:	%d %%' % (100 * correct / total))
 
+# Retrieves all indices
+def get_indice(indice = False):
+	all_indice = []
+	if indice is not False:
+		for line in indice:
+			all_indice.append(int(line))
+	return all_indice
+
+# Checks if window[start:end] is a continuous block
+def check_window(indice, start, end):
+	if len(indice) != 0:
+		array = indice[start:end]
+		for i, x in enumerate(array):
+			if i + 1 < len(array):
+				temp = x + 1
+				if temp == array[i+1]:
+					continue
+				else:
+					return False	# Window is not continuous
+	return True						# Window is continuous
+
 def kFold(data_file, indice_file):
 	data = np.loadtxt(data_file, delimiter=',', usecols=(1, 2, 3, 4, 5, 6, 7, 8, 9), dtype=np.float32)
+	
 	with open(indice_file, 'r') as f:
    		indice = [line.strip() for line in f]
-	return data, indice
 
-data, indice = kFold('./data/part1/Subject_2_part1.csv', './data/part1/list2_part1.csv')
+	data_total_len = data.shape[0]
+	all_indice = get_indice(indice)
 
-train_set = DiabetesDataset(data, indice)
+	batch = []
+	labels = []
+
+	for i, row in enumerate(data):
+		new_batch = []
+		if  i+7 <= data_total_len and check_window(all_indice, i, i+7):
+			for j in range(i, i+7):
+				new_batch = [x for x in itertools.chain(new_batch, data[j, 0:8])]
+				if j == i+6:
+					last = data[j, [-1]]
+			batch.append(new_batch)
+			labels = np.append(labels, last)
+		else:
+			continue			# If window size is not 7 or if the contents of the window is not continuous, skip
+
+	batch = np.array(batch)
+	test_data = batch
+	test_labels = labels
+	train_data = None
+	train_labels = None
+
+	return test_data, test_labels, train_data, train_labels
+
+test_data, test_labels, train_data, train_labels = kFold('./data/part1/Subject_2_part1.csv', './data/part1/list2_part1.csv')
+
+train_set = DiabetesDataset(test_data, test_labels)
 
 train_loader = DataLoader(dataset=train_set,
 						  batch_size=1,
 						  shuffle=True,
 						  num_workers=2)
 '''
-test_set = DiabetesDataset('./data/part1/Subject_7_part1.csv', './data/part1/list_7_part1.csv')
+test_set = DiabetesDataset(train_data, train_labels)
 validation_loader = DataLoader(dataset=test_set,
 						  batch_size=1,
 						  shuffle=False,
