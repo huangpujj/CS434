5891e44a9d08f64ec02861e260b53419e9b39044
trelkins3 <trelkins3@gmail.com>
2018-06-11 19:34:55 -0700

    Weights changing, gradient isn't

--- final_project/lc.py
+++ final_project/lc.py
@@ -31,9 +31,9 @@ def check_window(indice, start, end):
 
 def load_data(data_file, indice_file):
 	data = np.loadtxt(data_file, delimiter=',', usecols=(1, 2, 3, 4, 5, 6, 7, 8, 9), dtype=np.float32)
-	
+
 	with open(indice_file, 'r') as f:
-   		indice = [line.strip() for line in f]
+		indice = [line.strip() for line in f]
 
 	data_total_len = data.shape[0]
 	all_indice = get_indice(indice)
@@ -43,7 +43,7 @@ def load_data(data_file, indice_file):
 
 	for i, row in enumerate(data):
 		new_batch = []
-		if  (i+window_size <= data_total_len) and (check_window(all_indice, i, i+window_size)):
+		if(i+window_size <= data_total_len) and (check_window(all_indice, i, i+window_size)):
 			for j in range(i, i+window_size):
 				new_batch = [x for x in itertools.chain(new_batch, data[j, 0:8])]
 				if j == i+window_size-1:
@@ -64,47 +64,51 @@ def kFold(batch, labels):
 		y_train, y_test = labels[train_index], labels[test_index]
 
 	return X_train, y_train, X_test, y_test
-	
+
 ''' End of Parsing Code '''
 
 ''' Classifier Code '''
 # Might actually need this to be L2 and descent?
 def weight(batch, labels):
-    return (np.transpose(batch).dot(labels)).dot(np.linalg.inv(np.transpose(batch).dot(batch)))   # W = (X^T * X)^-1 * X^T * Y  
+	return (np.transpose(batch).dot(labels)).dot(np.linalg.inv(np.transpose(batch).dot(batch)))   # W = (X^T * X)^-1 * X^T * Y  
 
 def sigmoid(w, f):
-    return 1.0 / (1.0 + np.exp((-1.0 * np.transpose(w)).dot(f)))                    # 1 / (1 + e^(-w^T x))
+	return 1.0 / (1.0 + np.exp((-1.0 * np.transpose(w)).dot(f)))                    # 1 / (1 + e^(-w^T x))
 
 def gradient(w, f, o, lam = 0):
-    g = np.zeros(56, dtype=float)
-    for i in range(f.shape[0]):
-        y_hat = sigmoid(w, f[i])                # Iterate over all features in each row
-        if lam != 0:                            # If there is a lamda value then we're doing regularization for Part 2.3
-            y_hat = y_hat + (lam * np.linalg.norm(w, 2))
-        g = g + (float(o[i])) * f[i]    # Reversed on slides, does't work for y_hat - o[i]
-    return g
+	g = np.zeros(56, dtype=float)
+	for i in range(f.shape[0]):
+		y_hat = sigmoid(w, f[i])                # Iterate over all features in each row
+		if lam != 0:                            # If there is a lamda value then we're doing regularization for Part 2.3
+			y_hat = y_hat + (lam * np.linalg.norm(w, 2))
+		g = g + (float(o[i]) - y_hat) * f[i]    # Reversed on slides, does't work for y_hat - o[i]
+	return g
 
 def batch_gradient_descent(itr, learning_rate, f_train, o_train, f_test, o_test):
-    f = open("gradient_descent.csv", 'w+')
-    print("Iteration\tTraining Accuracy\tTest Accuracy")
-    f.write("Iteration,Training Accuracy,Test Accuracy\n")
+	f = open("gradient_descent.csv", 'w+')
+	print("Iteration\tTraining Accuracy\tTest Accuracy")
+	f.write("Iteration,Training Accuracy,Test Accuracy\n")
 
-    w = np.zeros(56, dtype=float)                      # Initilize w = [0, ...0]
-    
-    for i in range(1, itr):
-        g = gradient(w, f_train, o_train)
-        w = w + (learning_rate * g)
-        print(str(i) + "\t" + str(check(w, f_train, o_train)))
-        #f.write(str(i) + "," + str(check(w, f_train, o_train)) + "," + str(check(w, f_test, o_test)) + "\n")
+	w = np.zeros(56, dtype=float)                      # Initilize w = [0, ...0]
+
+	for i in range(1, itr):
+		g = gradient(w, f_train, o_train)
+		print("g: ")
+		print(g)
+		w = w + (learning_rate * g)
+		print("w: ")
+		print(w)
+		print(str(i) + "\t" + str(check(w, f_train, o_train)))
+		#f.write(str(i) + "," + str(check(w, f_train, o_train)) + "," + str(check(w, f_test, o_test)) + "\n")
 	f.close()
 
 def check(w, f, expected):  # Check predicted values agaist the correct value column and take the ratio of correct / total
-    correct = 0
-    for i in range(0, f.shape[0]):
-        y_hat = sigmoid(w, f[i])
-        if np.round(y_hat) == expected[i]:
-            correct += 1
-    return float(correct) / float(f.shape[0])   # Ratio expresses this weight's accuracy
+	correct = 0
+	for i in range(0, f.shape[0]):
+		y_hat = sigmoid(w, f[i])
+	if np.round(y_hat) == expected[i]:
+		correct += 1
+	return float(correct) / float(f.shape[0])   # Ratio expresses this weight's accuracy
 
 def print_data(train_data, train_labels, test_data, test_labels):
 	print "Training Data"
@@ -124,8 +128,8 @@ def print_data(train_data, train_labels, test_data, test_labels):
 
 s2_batch, s2_label = load_data('./data/part1/Subject_2_part1.csv', './data/part1/list2_part1.csv')
 
-itr = 100
-learning_rate = 0.0001
+itr = 10
+learning_rate = 0.01
 
 train_data, train_labels, test_data, test_labels = kFold(s2_batch, s2_label)
 
